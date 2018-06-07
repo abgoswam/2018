d645658ab4bfb440d840a4661b0b8818fca7d422
Abhishek Goswami <abgoswam@gmail.com>
2018-06-07 15:10:38 -0700

    solving algo assignment #2

--- 062018/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ 062018/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,6 +1,7 @@
 # Uses python3
 import sys
 
+
 def fibonacci_sum_naive(n):
     if n <= 1:
         return n
@@ -15,7 +16,28 @@ def fibonacci_sum_naive(n):
 
     return sum % 10
 
+
+def fibonacci_sum_fast(n):
+    if n <= 1:
+        return n
+
+    fib_mod10 = [0, 1]
+    fib_cum_mod10 = [0, 1]
+    while True:
+        x = (fib_mod10[-1] + fib_mod10[-2]) % 10
+        y = (x + fib_cum_mod10[-1]) % 10
+
+        fib_mod10.append(x)
+        fib_cum_mod10.append(y)
+
+        if fib_mod10[-2:] == [0, 1]:
+            seq_length = len(fib_mod10) - 2
+            break
+
+    return fib_cum_mod10[n % seq_length]
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(fibonacci_sum_naive(n))
+    n = int(input())
+    # print(fibonacci_sum_naive(n))
+    print(fibonacci_sum_fast(n))
