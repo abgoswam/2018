2db20b17525e2c2ba8f6484ceab93365a1cd8294
Abhishek Goswami <abgoswam@gmail.com>
2018-06-09 14:55:27 -0700

    solving algo assignment #3

--- 062018/week3_greedy_algorithms/1_money_change/change.py
+++ 062018/week3_greedy_algorithms/1_money_change/change.py
@@ -3,8 +3,18 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+
+    n_10 = m // 10
+    r_10 = m % 10
+
+    n_5 = r_10 // 5
+    r_5 = r_10 % 5
+
+    n_1 = r_5
+
+    return n_10 + n_5 + n_1
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
