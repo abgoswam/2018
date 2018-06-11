49eb20af9e028784933fdf38304ac5d97ca8bf3a
Abhishek Goswami <abgoswam@gmail.com>
2018-06-11 10:34:56 -0700

    solving algo assignment #3

--- 062018/week3_greedy_algorithms/3_maximum_advertisement_revenue/dot_product.py
+++ 062018/week3_greedy_algorithms/3_maximum_advertisement_revenue/dot_product.py
@@ -4,10 +4,15 @@ import sys
 
 def max_dot_product(a, b):
     #write your code here
+
+    a = sorted(a, reverse=True)
+    b = sorted(b, reverse=True)
+
     res = 0
     for i in range(len(a)):
         res += a[i] * b[i]
     return res
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -16,4 +21,3 @@ if __name__ == '__main__':
     a = data[1:(n + 1)]
     b = data[(n + 1):]
     print(max_dot_product(a, b))
-    
